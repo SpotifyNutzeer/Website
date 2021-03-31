0396092d6374f311ab8389128da44dc2882eea31
GermanJag <julian@gransee.me>
2021-03-31 02:01:05 +0200

    added basic structure

---
+++ index.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @author Julian Amadeus Gransee
+ * @package me.gransee.client.spotifyNutzer
+ * @copiright Julian G.
+ */
+
+/**
+ * TODO: better color scheme (dark theme)
+ * TODO: smooth scrolling
+ * TODO: remove cdn's
+ * TODO: contact Form?
+ * TODO: some content to work with
+ * TODO: link to the github pages...
+ * TODO: add site description
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<!--
+!-->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Paul</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/contact.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
+    <link rel="stylesheet" href="/assets/css/navigation.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean">
+        <div class="container"><a class="navbar-brand" href="#">SpotifyNutzer & Mikka </a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="#about">Über uns</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#projects">Projekte</a></li>
+                  <!--  <li class="nav-item"><a class="nav-link" href="#contact">Kontakt</a></li> !-->
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container about" id="about" >
+        <div class="row">
+            <div class="col">
+                <div>
+                    <h1>Heyyy</h1>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div>
+                    <p></p>
+                    <p>Hier könnt ihr etwas über euch schreiben<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>so lange, wie ihr wollt</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container projects" id="projects">
+        <div class="row">
+            <div class="col-md-4">
+
+                <div class="card-border">
+
+                    <div class="card-holder">
+
+                        <div class="card-image">
+                        </div>
+                        <div class="card-heading">
+                            <h1>
+                                und ja
+                            </h1>
+                        </div>
+                        <div class="card-text">
+                            <p>und ja </p>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-md-4">
+
+                <div class="card-border">
+
+                    <div class="card-holder">
+
+                        <div class="card-image">
+                        </div>
+                        <div class="card-heading">
+                            <h1>
+                                und ja
+                            </h1>
+                        </div>
+                        <div class="card-text">
+                            <p>und ja </p>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div><div class="col-md-4">
+
+            <div class="card-border">
+
+                <div class="card-holder">
+
+                    <div class="card-image">
+                    </div>
+                    <div class="card-heading">
+                        <h1>
+                            und ja
+                        </h1>
+                    </div>
+                    <div class="card-text">
+                        <p>und ja </p>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </div>
+    <!-- vielleicht ein contact form, vielleicht auch nicht
+    <section class="contact-clean contact" id="contact">
+        <form method="post">
+            <h2 class="text-center">Kontakt</h2>
+            <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+            <div class="form-group"><input class="form-control is-invalid" type="email" name="email" placeholder="Email"><small class="form-text text-danger">Please enter a correct email address.</small></div>
+            <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
+            <div class="form-group"><button class="btn btn-primary" type="submit">send </button></div>
+        </form>
+    </section>
+    !-->
+    <footer class="footer-basic">
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="/impressum">Impressum</a></li>
+            <li class="list-inline-item"><a href="/datenschutz">Datenschutzerklärung</a></li>
+        </ul>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
