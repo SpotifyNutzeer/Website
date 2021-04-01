ab5cd989bdd6d82d7f300e0a310310eb2f0b19ad
GermanJag <julian@gransee.me>
2021-04-01 02:11:41 +0200

    added socials and some new fancy css

---
+++ index.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * @author Julian Gransee
+ * @package me.gransee.client.spotifyNutzer
+ */
+
+/**
+ * TODO: remove cdn's
+ * TODO: contact Form?
+ * TODO: some content to work with
+ * TODO: add site description
+ * TODO: fix some css
+ */
+
+$title = 'Paul';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Paul</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/contact.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+<nav class="navbar navbar-light navbar-expand-md navigation">
+    <div class="container"><a class="navbar-brand" href="#">SpotifyNutzer & Mikka </a>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+                    class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="#about">Über uns</a></li>
+                <li class="nav-item"><a class="nav-link" href="#projects">Projekte</a></li>
+                <!--  <li class="nav-item"><a class="nav-link" href="#contact">Kontakt</a></li> !-->
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container about" id="about">
+    <div class="row">
+        <div class="col">
+            <div>
+                <h1>Heyyy</h1>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div>
+                <p></p>
+                <p>Hier könnt ihr etwas über euch
+                    schreiben<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>so lange, wie ihr wollt
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="container projects" id="projects">
+    <div class="row">
+        <div class="heading">
+            <h1></h1>
+        </div>
+        <div class="col-md-4">
+
+            <div class="card-border">
+
+                <div class="card-holder">
+
+                    <div class="card-image">
+                    </div>
+                    <div class="card-heading">
+                        <h1>
+                            und ja
+                        </h1>
+                    </div>
+                    <div class="card-text">
+                        <p>und ja </p>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col-md-4">
+
+            <div class="card-border">
+
+                <div class="card-holder">
+
+                    <div class="card-image">
+                    </div>
+                    <div class="card-heading">
+                        <h1>
+                            und ja
+                        </h1>
+                    </div>
+                    <div class="card-text">
+                        <p>und ja </p>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col-md-4">
+
+            <div class="card-border">
+
+                <div class="card-holder">
+
+                    <div class="card-image">
+                    </div>
+                    <div class="card-heading">
+                        <h1>
+                            und ja
+                        </h1>
+                    </div>
+                    <div class="card-text">
+                        <p>und ja </p>
+                    </div>
+                    <div class="card-link">
+                        <a href="http://linktoProject/">
+                            <button>Project</button>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="socials">
+    <div class="container">
+        <div class="socials-heading">
+
+            <h1>ich weiß nicht....</h1>
+
+        </div>
+        <div class="row">
+
+
+            <div class="col-md-6 socials-spotifynutzer">
+                <div class="socials-innerheading">
+                    <h2>Paul</h2>
+                </div>
+                <div class="socials-list">
+
+                    <a href="https://github.com/SpotifyNutzeer" target="_blank">
+                        <div class="socials-github">
+
+                            <div class="github">
+                                <i class="fab fa-github"></i>
+                                <p id="test" class="socials-text">SpotifyNutzeer auf GitHub</p>
+                            </div>
+
+
+                        </div>
+                    </a>
+
+                    <a href="https://twitter.com/SpotifyNutzer" target="_blank">
+                        <div class="socials-twitter">
+
+                            <div class="twitter">
+                                <i class="fab fa-twitter"></i>
+                                <p id="test" class="socials-text">SpotifyNutzer auf Twitter</p>
+                            </div>
+
+
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 socials-kxmpetentes">
+
+                <div class="socials-innerheading">
+                    <h2>nicht Paul</h2>
+                </div>
+
+                <div class="socials-list">
+
+                    <a href="https://github.com/kxmpetentes" target="_blank">
+                        <div class="socials-github">
+
+                            <div class="github">
+                                <i class="fab fa-github"></i>
+                                <p id="test" class="socials-text">kxmpetentes auf GitHub</p>
+                            </div>
+
+
+                        </div>
+                    </a>
+
+                    <a href="https://twitter.com/kxmpetentes" target="_blank">
+                        <div class="socials-twitter">
+
+                            <div class="twitter">
+                                <i class="fab fa-twitter"></i>
+                                <p id="test" class="socials-text">kxmpetentes auf Twitter</p>
+                            </div>
+
+
+                        </div>
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- vielleicht ein contact form, vielleicht auch nicht
+<section class="contact-clean contact" id="contact">
+    <form method="post">
+        <h2 class="text-center">Kontakt</h2>
+        <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name"></div>
+        <div class="form-group"><input class="form-control is-invalid" type="email" name="email" placeholder="Email"><small class="form-text text-danger">Please enter a correct email address.</small></div>
+        <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
+        <div class="form-group"><button class="btn btn-primary" type="submit">send </button></div>
+    </form>
+</section>
+!-->
+<footer class="footer">
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="/impressum">Impressum</a></li>
+        <li class="list-inline-item"><a href="/datenschutz">Datenschutzerklärung</a></li>
+    </ul>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/2226ce608c.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
