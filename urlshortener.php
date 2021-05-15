baf72d9cdac6aa33e929201b78bc4e490d92812d
GermanJag <julian@gransee.me>
2021-05-15 09:55:24 +0200

    removed debugging

--- urlshortener.php
+++ urlshortener.php
@@ -1,7 +1,6 @@
 <?php
- ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+header('Content-Type: application/json');
+
 //temp
 $apiEndpoint = "https://api.spotifynutzer.xyz/";
 
